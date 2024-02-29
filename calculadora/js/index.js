4e78ca21ca0be430440072c6c9942279eac9455a
gbmedeiros00 <157700662+gbmedeiros00@users.noreply.github.com>
2024-02-29 09:06:46 -0300

    Delete calculadora directory

--- calculadora/js/index.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('#calcular').addEventListener('click', e =>{
-    let numero1 = document.querySelector('#valor1').value;
-    let numero2 = document.querySelector('#valor2').value;
-    let operador = document.querySelector('#operador').value;
-
-    let total = eval( `${numero1} ${operador} ${numero2}` );
-    console.log(total);
-});
